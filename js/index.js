5bf27a2e96fb87b61e120d63adcea880ff4f4cbd
caro <caro.rod935@gmail.com>
2022-12-29 20:26:52 -0300

    segunda correcion del responsive

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
-const BtnSend= document.getElementById('btnSend')
+const BtnSend= document.getElementById('btnSend');
 BtnSend.on('click', function(ev){
 
-    document.getElementById("txtName").value = "";
-    document.getElementById("txtEmail").value = "";
-    document.getElementById("txtMessage").value = "";
+
+
+    document.getElementById("txtName").val = "";
+    document.getElementById("txtEmail").val = "";
+    document.getElementById("txtMessage").val = "";
 
  
 });
