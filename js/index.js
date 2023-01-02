6d52599edc8a1446c5564a9fe3888389d1c58e4e
caro <caro.rod935@gmail.com>
2023-01-02 16:37:08 -0300

    arreglos finales en responsive e indexs

--- js/index.js
+++ js/index.js
@@ -1,11 +1,30 @@
-const BtnSend= document.getElementById('btnSend');
-BtnSend.on('click', function(ev){
+window.onbeforeunload = () => {
+    for(const form of document.getElementsByTagName('form')) {
+      form.reset();
+    }
+  }
 
 
+  const openEls = document.querySelectorAll("[data-open]");
+  const closeEls = document.querySelectorAll("[data-close]");
+  const esVisible= "es-visible";
+  
+  
+  
+  for (const el of openEls){
+  el.addEventListener("click", function(){
 
-    document.getElementById("txtName").val = "";
-    document.getElementById("txtEmail").val = "";
-    document.getElementById("txtMessage").val = "";
-
- 
-});
+      const modalId= this.dataset.open;
+      document.getElementById(modalId).classList.add(esVisible);
+  });
+  }
+  
+  
+  
+  
+  for (const el of closeEls){
+  el.addEventListener("click", function(){
+   
+  this.parentElement.parentElement.parentElement.classList.remove(esVisible);
+      });
+      }
