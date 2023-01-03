3391aad123003c7c54c256d1af4c35f85b355bea
caro <caro.rod935@gmail.com>
2023-01-02 23:39:38 -0300

    finalizacion del proyecto de portafolio

--- js/index.js
+++ js/index.js
@@ -3,28 +3,3 @@ window.onbeforeunload = () => {
       form.reset();
     }
   }
-
-
-  const openEls = document.querySelectorAll("[data-open]");
-  const closeEls = document.querySelectorAll("[data-close]");
-  const esVisible= "es-visible";
-  
-  
-  
-  for (const el of openEls){
-  el.addEventListener("click", function(){
-
-      const modalId= this.dataset.open;
-      document.getElementById(modalId).classList.add(esVisible);
-  });
-  }
-  
-  
-  
-  
-  for (const el of closeEls){
-  el.addEventListener("click", function(){
-   
-  this.parentElement.parentElement.parentElement.classList.remove(esVisible);
-      });
-      }
